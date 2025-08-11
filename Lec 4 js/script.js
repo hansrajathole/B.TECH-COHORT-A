7d0527890c524004ff3694df159f62cf075e81b8
hansrajathole <hansrajathole1@gmail.com>
2025-08-11 16:20:14 +0530

    js basic

---
+++ Lec 4 js/script.js
@@ -0,0 +1,81 @@
+
+// function Declaration
+// function sum() {
+//     let a = 10
+//     let b = 20 
+//     return a+b 
+// }
+
+
+// console.log(sum());
+
+
+
+
+// Function expression
+// let sum = function (){
+
+
+    
+// }
+
+// sum()
+// console.log(sum);
+
+
+
+
+// Arrow Function 
+// let sum = (a , b)=>{
+//     // let a = 10
+//     // let b = 20 
+//     console.log(a+b);
+    
+// }
+
+// sum(20 , 30)
+
+
+
+
+// function fun( age = 111 ,name = "Chacha") {
+//     console.log("hello from :", name);
+//     console.log("age is "+age);
+    
+// }
+
+// fun(68)
+
+// let arr = [10,20,30,40]
+
+// console.log(arr[0]);
+
+// arr.push(50)
+// let last = arr.pop()
+// console.log(last);
+
+// console.log(arr)
+
+
+
+
+// arr.shift()
+// let newArr = arr.unshift(5)
+// console.log(newArr);
+// console.log(arr);
+
+
+
+// console.log(arr.slice(0,2));
+
+
+// arr.splice(2,1)
+// console.log(arr);
+
+
+
+// console.log(arr.indexOf(40));
+// console.log(arr.includes(35));
+
+
+
