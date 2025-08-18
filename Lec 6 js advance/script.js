d00d3ddaebb616666a3d2df4bc1427e34a6e3aa4
hansrajathole <hansrajathole1@gmail.com>
2025-08-18 16:22:54 +0530

    js advance

---
+++ Lec 6 js advance/script.js
@@ -0,0 +1,121 @@
+
+// console.log(this);
+
+
+// let  person = {
+//     name : "Rani kumari",
+//     age : 21,
+//     mo : 9786785265,
+//     gander : "female",
+//     address : {
+//         state : "MP",
+//         city : "indore",
+//         pinCode : 45331
+//     },
+
+//     fun(){
+   
+//         return "hello from "+this["full name"]
+        
+//     }
+// }
+
+// let {name, age , address } = person
+// let name = person.name
+// let age = person.age
+// let address = person.address
+
+
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+
+
+// console.log(person.full name);
+// console.log(person["full name"]);
+
+
+// person.age = 22
+// person.address.city = "Bhopal"
+
+// console.log(person);
+
+// let returnValue = person.fun()
+// console.log(returnValue);
+
+
+// let date = new Date()
+// console.log(date);
+// console.log(date.getMonth());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getMilliseconds());
+// console.log(date.getMinutes());
+
+// console.log(5/2);
+// console.log(Math.floor(5/2));
+// console.log(Math.ceil(5/2));
+
+
+// console.log(Math.floor(Math.random()*100));
+
+// console.log(Math.min(40,100)); //40
+// console.log(Math.max(40,100)); // 100
+
+
+
+
+// let arr = [10,20,30,40,50,60]
+
+
+
+// let copyArr = [...arr]
+// copyArr.push(70)
+
+// console.log(arr);
+// console.log(copyArr);
+
+
+
+// function sum(...numbers){
+//    let sum = 0
+//    for(let i = 0 ; i< numbers.length ; i++){
+//      sum += numbers[i]
+//    }
+
+//    console.log(sum);
+   
+// }
+
+
+// sum(10,20,30,40,50)
+
+
+
+// let arr = [10,20,30,40,50,60]
+
+// for (const elem of arr) {
+//     console.log(elem);
+    
+// }
+
+
+
+
+
+// let  person = {
+//     name : "Rani kumari",
+//     age : 21,
+//     mo : 9786785265,
+//     gender : "female"
+// }
+
+// for (const key in person) {
+  
+//     console.log(key ,"==>",person[key]);
+    
+    
+// }
