1e534b2ba8cfb8b02304bceb2eea55bc8f04a818
hansrajathole <hansrajathole1@gmail.com>
2025-09-01 16:10:51 +0530

    Add HTML structure, CSS styles, and JavaScript functionality for interactive elements

---
+++ Lec 10 DOM/script.js
@@ -0,0 +1,42 @@
+
+// getAtrribute and setAttribute 
+// const btn = document.querySelector("button")
+// btn.addEventListener("click", ()=>{
+//     let img1 = document.querySelector("#one")
+//     let img2 = document.querySelector("#two")
+
+//     let imgSrc1 = img1.getAttribute("src")
+//     let imgSrc2 = img2.getAttribute("src")
+
+//     img1.setAttribute("src",imgSrc2)
+//     img2.setAttribute("src", imgSrc1)
+// })
+
+
+
+// let inp = document.querySelector("input")
+
+
+// inp.addEventListener("input", (event)=>{
+//     // console.log(event);
+//     // console.log(event.target);
+//     console.log(event);
+    
+// })
+
+
+let select = document.querySelector('select')
+
+// select.addEventListener("input", (event)=>{
+//     console.log(event.target.selected);
+// })
+
+
+// const form = document.querySelector("form")
+
+
+// form.addEventListener("submit", (event)=>{
+//     event.preventDefault()
+//     console.log("ab reload nhi hua");
+    
+// })
